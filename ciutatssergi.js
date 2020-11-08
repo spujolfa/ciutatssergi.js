01fd8e5f0d34d740da33edf4b234d64a5e59192a
Ismael <ispernava@gmail.com>
2020-11-08 16:57:39 +0100

    Arreglar bug del fitxer

--- ciutatssergi.js
+++ ciutatssergi.js
@@ -1,5 +1,5 @@
 function PrintCities() {
-    var a = ["Lleida", Barcelona", "Paris"];
+    var a = ["Lleida", "Barcelona", "Paris"];
     a.forEach(function(entry) {
         console.log(entry);
     });
